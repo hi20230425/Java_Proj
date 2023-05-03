af2a4fae7f3bc0f254c3f35abf9adc1559f85294
Jangwoo Park <p.jangwoo@gmail.com>
2023-05-03 12:42:17 +0900

    첫번째 커밋 - 2023_05_03

---
+++ src/hi/org/Helloworld.java
@@ -0,0 +1,30 @@
+package hi.org;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// 메인 메소드 에서 프로그램 작성 
+		// 한줄 주석 : 프로그램에서 처리되지 않는 블락, 설명
+		/*
+		   여러줄 주석 
+		 */
+		
+		System.out.println("Hello World ~!!!!");
+		
+		// 출력 후 라인을 개행하라 
+		System.out.println("두번째 출력 구문 ~~~~");
+		// syso + <ctrl + spacebar> 
+		
+		
+		//출력하라   (Enter 가 적용되지 않는다. )
+		System.out.print("안녕 ");
+		System.out.print("하세요");
+		
+		//   \n : enter 
+		System.out.print("안녕 \n ");
+		System.out.print("하세요");
+		
+
+	}
+
+}
