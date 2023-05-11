618070da85f9755436c97829dff9d5281568701c
Jangwoo Park <p.jangwoo@gmail.com>
2023-05-11 15:13:40 +0900

    5월 11일 - 3시

--- src/chapter05/ex04/Ex01.java
+++ src/chapter05/ex04/Ex01.java
@@ -24,26 +24,34 @@ public class Ex01 {
 		for ( int i = 0 ; i < arr1.length; i++) {		//i : 행을 Loop : 0, 1, 2, 3
 				if (i == 0 ) {		// 10의 배수를 저장함.
 					for ( int j = 0, a=10 ; j < arr1[i].length; j++ ) {  //j : 열을 Loop : 0, 1, 2, 3, .....99
-						// 3의 배수를 저장하는 변수 선언 
+						// 10의 배수를 저장하는 변수 선언 
 	 
 						arr1[i][j] = a;
 						a +=10; 
 					}
 					
 				}else if ( i==1 ) { //3의 배수만 저장 
-					for ( int j = 0 ; j < arr1[i].length; j++ ) {  //j : 열을 Loop : 0, 1, 2, 3, .....99
-						
+					for ( int j = 0 , a = 3 ; j < arr1[i].length; j++, a+=3 ) {  //j : 열을 Loop : 0, 1, 2, 3, .....99
+						// 3의 배수를 저장하는 변수 선언 
+						//int a = 3; 
+						arr1[i][j] = a; 
+						//a+=3; 
 						
 					}
-				}else if ( i ==2) {  //3배수지만 6의 배수를 저장하지 말고 저장
-					for ( int j = 0 ; j < arr1[i].length; j++ ) {  //j : 열을 Loop : 0, 1, 2, 3, .....99
+				}else if ( i == 2) {  //3배수지만 6의 배수를 저장하지 말고 저장
+					for ( int j = 0, a=3 ; j < arr1[i].length; a+=3) {  //j : 열을 Loop : 0, 1, 2, 3, .....99
 						
-						
+						if ( a % 6 == 0 ) continue;    // 6의 배수는 저장하지 않고 
+						arr1[i][j] = a; 
+						j++;
 					}
 				}else if ( i == 3) { //7의 배수, 8의 배수 저장
-					for ( int j = 0 ; j < arr1[i].length; j++ ) {  //j : 열을 Loop : 0, 1, 2, 3, .....99
-						
-						
+					for ( int j = 0,  a = 1 ; j < arr1[i].length;  a++ ) {  //j : 열을 Loop : 0, 1, 2, 3, .....99
+						// 각 방의 값을 저장하는 변수 선언 : a ; 
+						if ( a % 7 == 0 || a % 8 == 0 ) {
+							arr1[i][j] = a; 
+							j++; 
+						}
 					}
 				}
 				
---
+++ src/chapter05/ex05/Rectangle_Array_Output.java
@@ -0,0 +1,27 @@
+package chapter05.ex05;
+
+public class Rectangle_Array_Output {
+
+	public static void main(String[] args) {
+		/*
+		    2차원 -  정방형 배열 출력 방법 
+		     	- For - For
+		     	- Enhanced For - Enhanced For 
+		     	- Arrays.toString(배열 변수) 
+		 */
+		
+		//배열 : 행 : 3  , 열 : 100 ( index : 0 ~ 99 )   :  for - for  
+			// 0 행  - 1 ~ 100 까지 정수 
+			// 1 행  - 2의 배수 
+		    // 2 행  - 3의 배수 
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
