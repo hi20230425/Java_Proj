ed6d205ff0162b646dbd5f0330e63a9472d0d620
Jangwoo Park <p.jangwoo@gmail.com>
2023-05-24 18:33:37 +0900

    5월 24일 저녁

--- src/chapter10/ex07/Ex01.java
+++ src/chapter10/ex07/Ex01.java
@@ -22,11 +22,11 @@ class Banana {
 		this.count = count; 
 	}
 }
-class Stroberry {
+class Strawberry {
 	String name;   // 이름
 	int price ; 	//가격
 	int count; 		//갯수 
-	Stroberry (String name, int price, int count) {
+	Strawberry (String name, int price, int count) {
 		this.name = name; 
 		this.price = price; 
 		this.count = count; 
@@ -37,6 +37,27 @@ public class Ex01 {
 	
 	// 배열의 객체을 매개변수로 받아서 총 가격을 출력 
 	void totalPrice (Object[] obj) {
+		//합계를 구하는 변수 선언 
+		int sumApple = 0 ;     //price * count 
+		int sumBanana = 0; 
+		int sumStrawberry = 0 ; 
+		
+		// 배열 변수로 선언 
+		int[] sum = new int[3];   // sum[0] : Apple , sum[1] : Banana, sum[2] : Strawberry
+		
+		if (obj[0] instanceof Apple) {
+			sum[0] = ((Apple) obj[0]).price *  ((Apple) obj[0]).count ; 
+		}
+		if ( obj[1] instanceof Banana) {
+			sum[1] = ((Banana) obj[1]).price *  ((Banana) obj[1]).count ; 
+		}
+		if ( obj[2] instanceof Strawberry) {
+			sum[2] = ((Strawberry) obj[2]).price *  ((Strawberry) obj[2]).count ; 
+		}
+		System.out.println("사과의 합은 : " + sum[0]);
+		System.out.println("바나나의 합은 : " + sum[1]);
+		System.out.println("딸기의 합은 : " + sum[2]);
+		System.out.println("모든 과일의 총합은 : " + ( sum[0] + sum[1] + sum[1] ) );
 		
 	}
 		
@@ -54,11 +75,21 @@ public class Ex01 {
 	
 		*/ 
 		
+		Apple a1 = new Apple ("사과", 2000,  5 ); 
+		Banana b1 = new Banana ("바나나", 4000 ,10);
+		Strawberry s1 = new Strawberry ("딸기", 5000 , 30);
+		
+		//배열선언후 값 할당. 
+		Object[] arr = new Object[] {a1, b1, s1}; 
+			//a1 : Object / Apple 
+			//b2 : Object / Banana
+			//s1 : Object / Strawberry 	
 		// 모든 객체를 Ojbect 배열에 넣습니다. 
 		
-		Object[] obj = new Object[] { } 
-		
-		
+		//메소드 호출 
+		Ex01 e1 = new Ex01(); 
+		e1.totalPrice(arr);
+			
 	}
 
 }
---
+++ src/chapter12/ex05/Fluit.java
@@ -0,0 +1,13 @@
+package chapter12.ex05;
+
+public interface Fluit {     // 과일 <인터페이스>    // Apple, Strawberry, Banana   // Fluit_test 
+	
+	// 17: 50 분까지 : 
+	
+	
+	void name(); 
+	void eat (); 
+	void color(); 
+	
+
+}
