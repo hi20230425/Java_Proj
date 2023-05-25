ea67942d55c49e4f02bb9234e43bbe456a4e2ff7
Jangwoo Park <p.jangwoo@gmail.com>
2023-05-25 18:16:36 +0900

    5월 25일 - 저녁

--- src/chapter12/ex05/Fluit.java
+++ src/chapter12/ex05/Fluit.java
@@ -3,11 +3,8 @@ package chapter12.ex05;
 public interface Fluit {     // 과일 <인터페이스>    // Apple, Strawberry, Banana   // Fluit_test 
 	
 	// 17: 50 분까지 : 
-	
-	
-	void name(); 
+	void name(); 	//public abstract 
 	void eat (); 
 	void color(); 
-	
 
 }
